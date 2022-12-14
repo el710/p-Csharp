1737b7ee4da3efc6975aa1f921ba4cf338795afe
Kim Oleg <theel710@gmail.com>
2022-12-14 22:45:16 +0300

    start zero project

---
+++ project_zero/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// data types
+bool bool_var;
+int int_num_var;
+float real_num_var;
+string text_var;
+
+
+
+
+
+Console.WriteLine("Hello, World!");
