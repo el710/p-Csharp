bd719720ed2d0ccefda90a83794967f281fb77da
el710 <el710@mail.ru>
2023-02-03 21:29:49 +0300

    add telegram bot

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,33 @@
+﻿// Telegram bot https://t.me/ElLittleBot
+
+using Telegram.Bot;
+
+string tokenTel = "5565110850:AAEpoukwdaiq9uOELSLzrFQeZR54kIyWGU8"; // from @BotFather
+var clientTel = new TelegramBotClient(tokenTel);
+
+string Answer(string msg){
+
+    string answer ="You say: " + msg;
+
+    if(msg.Contains("hello")){
+        answer = "Hi! ";
+    }
+
+    return answer;
+}
+
+
+clientTel.StartReceiving(
+ //   (c, args, _) => c.SendTextMessageAsync(args.Message.Chat.Id, "Hello!"),
+    (c, arg, _) =>
+    {
+       return c.SendTextMessageAsync(arg.Message.Chat.Id, 
+                                    Answer(arg.Message.Text.ToLower())
+       );
+    },
+    (_, _, _) => Task.CompletedTask
+);
+
+Console.WriteLine("Start");
+Console.ReadLine();
+
---
+++ TelegramBot/readme.md
@@ -0,0 +1,11 @@
+(ksergeyru) [https://boosty.to/ksergeyru/posts/657ba346-7f6f-4cf0-afa6-6c951d054526?share=post_link]
+
+
+##Make telegram bot
+
+1. In Telegram open @BotFather and click "start"
+2. Send command "/newbot"
+3. Make Name and username of bot and get Token
+4. Create dotnet console project
+5. Add package: <dotnet add package telegram.bot>
+6. Progamming...
